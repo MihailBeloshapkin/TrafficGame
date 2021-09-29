90493253e5f6bfb9bf4c42e715da273a9e8c7245
MihailBeloshapkin <mihail.beloshapkin.mm@yandex.ru>
2021-09-29 12:04:56 +0300

    some changes

--- road/Assets/Scripts/Conv.cs
+++ road/Assets/Scripts/Conv.cs
@@ -1,53 +1,106 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Conveer.
+/// </summary>
 public class Conv : MonoBehaviour
 {
-    public List<GameObject> Plates; 
+    /// Current plates
+    public List<GameObject> Road;
 
+    /// <summary>
+    /// Bool array. 
+    /// </summary>
+    public bool[] isRoad;
+
+    /// Direction. 
     public Vector3 platformDirection = new Vector3(0, 5, 0);
 
-    
-    public GameObject lastPlate;
+    public int size = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         int iter = 0;
-        foreach (var plat in Plates)
+        int roadIndex = 0;
+        isRoad = new bool[Road.Count];
+        foreach (var plat in Road)
         {
-            plat.transform.localPosition += new Vector3(0, 0, iter * 5);
+/*            plat.transform.localPosition += new Vector3(0, 0, roadIndex * 5);
+            roadIndex++;
+*/
+            if (iter % 2 == 0)
+            {
+                plat.transform.localPosition += new Vector3(0, 0, roadIndex * 5);
+                roadIndex++;
+                isRoad[iter] = true;
+            }
+            else
+            {
+                plat.transform.localPosition += new Vector3(0, -50, -50);
+                isRoad[iter] = false;
+            }
             iter++;
         }
-        lastPlate = Plates[0];
+        
     }
 
-    // Updates.
+    /// <summary>
+    /// Fixed update.
+    /// </summary>
     void FixedUpdate()
     {
         Move();
         TransformPlates();
     }
 
-    
-    
+    private int Rand(int size) => UnityEngine.Random.Range(0, size);
+
+    /// <summary>
+    /// Move plates.
+    /// </summary>
     private void TransformPlates()
     {
-        foreach (var plate in Plates)
+        
+        foreach (var plate in Road) 
         {
-            if (plate.transform.localPosition.z < Camera.main.transform.position.z)
+            if (isRoad[Road.IndexOf(plate)] && plate.transform.localPosition.z < Camera.main.transform.position.z) 
             {
-                plate.transform.localPosition += new Vector3(0, 0, Plates.Count * 5);
+            //    plate.transform.localPosition += new Vector3(0, 0, (Road.Count / 2) * 5);
+                var pos = plate.transform.localPosition;// += new Vector3(0, 0, Road.Count * 5);
+                var index = Road.IndexOf(plate);
+                int newPlateNumber = UnityEngine.Random.Range(0, (isRoad.Length / 2) - 1);
+                int j = 0;
+                int newIndex = 0;
+                for (int i = 0; i < Road.Count; i++) {
+                    if (!isRoad[i] && j == newPlateNumber)
+                        newIndex = i;
+                    if (!isRoad[i])
+                        j++;
+                }
+                plate.transform.localPosition += new Vector3(0, -50, -50);
+                Road[newIndex].transform.localPosition = pos + new Vector3(0, 0, (Road.Count / 2) * 5);
+                isRoad[index] = false;
+                isRoad[newIndex] = true;
+             
             }
         }
     }
 
+    /// <summary>
+    /// Move single plate.
+    /// </summary>
     private void Move()
     {
-        foreach (var plat in Plates)
+        foreach (var plat in Road)
         {
-            plat.transform.localPosition += platformDirection;   
+            if (isRoad[Road.IndexOf(plat)])
+            {
+                plat.transform.localPosition += platformDirection;
+            }
         }
     }
 }
